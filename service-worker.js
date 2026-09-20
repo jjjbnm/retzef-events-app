83de597c193f42dd9c4b569ecc5831734eb73c78
jjjbnm <3.0264799e+08+jjjbnm@users.noreply.github.com>
2026-09-20 19:30:41 +0000

    Revert "Add Yom Kippur availability notice"
    This reverts commit 1b4f4eee020466fffe217c27093ed0acb888bc5d.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='retzef-events-v4-yom-kippur-notice';
+const CACHE_NAME='retzef-events-v3-birthday-one-year';
 const APP_SHELL=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
